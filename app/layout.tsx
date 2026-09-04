3939adc92a7993fc80a297274cc917e7daae36b1
Srikar Mahankali <srikar.primary@gmail.com>
2026-09-04 12:43:34 +0800

    Added new icon for tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,20 +23,7 @@ export const metadata: Metadata = {
   title,
   description,
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/icon-32x32.png',
     apple: '/apple-icon.png',
   },
   openGraph: {
